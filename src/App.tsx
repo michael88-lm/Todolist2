a2184aac493185a94dedca75b093c40a0a190487
michael88-lm <mixail29.09.2021@yandex.by>
2024-09-22 19:17:03 +0300

    remove

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ function App() {
     { id: 1, title: "HTML&CSS", isDone: true },
     // { id: 2, title: "JS", isDone: true },
     { id: 3, title: "ReactJS", isDone: false },
-    { id: 3, title: "ReactJS", isDone: false },
     { id: 4, title: "Redux", isDone: false },
     { id: 5, title: "Typescript", isDone: false },
     { id: 6, title: "RTK query", isDone: false },
